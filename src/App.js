dc6dc239c2270ea0deca0136b1a4c9006fb6c63f
Evandro Carreira <evandro.carreira@gmail.com>
2019-02-07 17:01:02 -0200

    Adding QRReader

--- src/App.js
+++ src/App.js
@@ -1,27 +1,58 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import QrReader from 'react-qr-reader'
 import './App.css';
 
 class App extends Component {
+  state = {
+    result: '',
+    status: 'display'
+  }
+
+  handleScan = data => {
+    if (data) {
+      this.setState({
+        result: data,
+        status: 'display'
+      })
+    }
+  }
+  handleError = err => {
+    console.error(err)
+  }
+
+  changeStatus = (status) => {
+    this.setState({
+      status
+    })
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.status === 'read' && (
+        <div className="qr-reader">
+          <QrReader
+            delay={100}
+            onError={this.handleError}
+            onScan={this.handleScan}
+          />
+          <button onClick={() => this.changeStatus('display')}>Cancel</button>
+        </div>
+        )}
+        {this.state.status === 'display' && this.state.result !== '' && (
+        <div>
+          <h4>Your QR Code says</h4>
+          <h1>{this.state.result}</h1>
+          <button onClick={() => this.changeStatus('read')}>Scan again</button>
+        </div>
+        )}
+        {this.state.status === 'display' && this.state.result === '' && (
+        <div>
+          <button onClick={() => this.changeStatus('read')}>Scan QR Code</button>
+        </div>
+        )}
       </div>
-    );
+    )
   }
 }
 
